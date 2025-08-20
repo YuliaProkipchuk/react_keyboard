c36f4c2a824d47e4b7154fab7dcae56750592816
YuliaProkipchuk <yulayulayula2003@gmail.com>
2025-08-20 09:49:41 +0300

    add keyboard press functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: string;
+};
+export class App extends React.Component<State> {
+  state: State = {
+    pressedKey: '',
+  };
+
+  keyPress = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  componentDidMount(): void {
+    window.addEventListener('keyup', this.keyPress);
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener('keyup', this.keyPress);
+  }
+
+  render(): React.ReactNode {
+    const { pressedKey } = this.state;
+    const message = pressedKey
+      ? `The last pressed key is [${pressedKey}]`
+      : 'Nothing was pressed yet';
+
+    return (
+      <div className="App">
+        <p className="App__message">{message}</p>
+      </div>
+    );
+  }
+}
